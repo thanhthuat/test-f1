554b4bb8e58ab02e880dcca8b617023eeae4361b
thanhthuat <thanhthuat07@gmail.com>
2023-04-04 11:27:23 +0700

    add lib

--- components/carosels/carosel.tsx
+++ components/carosels/carosel.tsx
@@ -1,11 +1,11 @@
  import  Button  from '@mui/material/Button'
 import  Box from '@mui/material/Box'
 import React from 'react'
-import  Tab  from '@mui/material/Tab'
 
-interface Props  {}
 
-const Carosel:React.FC<Props> = () => {
+interface IpropsCarosel  {}
+
+const Carosel:React.FC<IpropsCarosel> = () => {
   return (
     <div className='home'>carosel
     <Box>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '@styles/Home.module.css'
-import '../public/scss/index.scss'
+
+import '../styles/scss/index.scss'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
