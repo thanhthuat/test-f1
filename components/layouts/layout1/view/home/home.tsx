4c45d6f543419e8703d52e4b7958623a5fd2d872
thanhthuat <thanhthuat07@gmail.com>
2023-05-12 16:42:53 +0700

    update

--- components/layouts/layout1/view/home/home.tsx
+++ components/layouts/layout1/view/home/home.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import HeaderTop from "../../header-top/header";
-import HeaderLayout1 from "../../header/header-layout1";
 import ContentHome from "../../content-home/content-home";
 import MainLayout from "../../hoc/main-layout";
 
